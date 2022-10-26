1dbdadb01d02897b12738415f373eb15c689f914
Jonathan Blades <jonoblades@gmail.com>
2022-10-26 11:07:59 +0100

    Added tests

---
+++ src/mocks/links.mock.ts
@@ -0,0 +1,22 @@
+const linksMock = [
+  {
+    _id: '1',
+    categories: ['Test'],
+    title: 'A link',
+    url: 'https://google.co.uk',
+    createdAt: '2022-10-07T13:07:05.240Z',
+    updatedAt: '2022-10-07T13:07:05.240Z',
+    __v: 0,
+  },
+  {
+    _id: '2',
+    categories: ['Test'],
+    title: 'Another link',
+    url: 'https://microsoft.com',
+    createdAt: '2022-10-07T13:07:05.240Z',
+    updatedAt: '2022-10-07T13:07:05.240Z',
+    __v: 0,
+  }
+];
+
+export default linksMock;
